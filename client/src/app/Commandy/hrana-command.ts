467ff563ada8de3f1218b904e7b4d881092f667e
Rado <you@example.com>
2019-06-14 19:26:26 +0200

    Zjednodusena metoda undo pri HranaCommande

--- client/src/app/Commandy/hrana-command.ts
+++ client/src/app/Commandy/hrana-command.ts
@@ -19,6 +19,40 @@ export class HranaCommand implements Command{
 
   }
 
+  set_suradnice_polyline_fakeVrchol(fake,points){
+    if(fake.typ_multipola == "multipol4") {
+      points.push(new fabric.Point(fake.suradnicaLeft + 125, fake.suradnicaTop + 125));
+    }
+    if(fake.typ_multipola == "multipol2") {
+      points.push(new fabric.Point(fake.suradnicaLeft + 125, fake.suradnicaTop + 47.5));
+    }
+    if(fake.typ_multipola == "multipol3") {
+      points.push(new fabric.Point(fake.suradnicaLeft + 117.5, fake.suradnicaTop + 120));
+    }
+    if(fake.typ_multipola == "multipol5") {
+      points.push(new fabric.Point(fake.suradnicaLeft + 117.5, fake.suradnicaTop + 117.5));
+    }
+  }
+
+  create_polyline(points){
+    this.polyLine = new fabric.Polyline(points, {
+      stroke: 'black',
+      strokeWidth:5,
+      selectable:false,
+    });
+    this.polyLine.set("type","edge");          //tu bol type hrana
+    this.prvy.set("pocetHran",this.prvy.pocetHran+1);
+    this.druhy.set("pocetHran",this.druhy.pocetHran+1);
+    Informacie.plocha.add(this.polyLine);
+    Informacie.plocha.moveTo(this.polyLine, -100);
+
+    this.prvy.lockMovementX=true;
+    this.prvy.lockMovementY=true;
+    this.druhy.lockMovementX=true;
+    this.druhy.lockMovementY=true;
+    Informacie.hranyVGrafe.push(this.hranaJS);
+  }
+
   //pridanie novej hrany, suradnice polyline sa vypocitavaju podla toho kolko ma multipol vytrcajucich hran
   execute() {
     const points = [];
@@ -29,18 +63,7 @@ export class HranaCommand implements Command{
       Informacie.plocha.remove(Informacie.vizitka);
       Informacie.poleMultipolov[this.prvy.multipol][0].off("mousedblclick");
 
-      if(this.prvy.typ_multipola == "multipol4") {
-        points.push(new fabric.Point(this.prvy.suradnicaLeft + 125, this.prvy.suradnicaTop + 125));
-      }
-      if(this.prvy.typ_multipola == "multipol2") {
-        points.push(new fabric.Point(this.prvy.suradnicaLeft + 125, this.prvy.suradnicaTop + 47.5));
-      }
-      if(this.prvy.typ_multipola == "multipol3") {
-        points.push(new fabric.Point(this.prvy.suradnicaLeft + 117.5, this.prvy.suradnicaTop + 120));
-      }
-      if(this.prvy.typ_multipola == "multipol5") {
-        points.push(new fabric.Point(this.prvy.suradnicaLeft + 117.5, this.prvy.suradnicaTop + 117.5));
-      }
+      this.set_suradnice_polyline_fakeVrchol(this.prvy,points);
       let index_of_group = Informacie.poleMultipolov[this.prvy.multipol].length-1;
       Informacie.poleMultipolov[this.prvy.multipol][0].item(0).set("fill",Informacie.poleMultipolov[this.prvy.multipol][index_of_group].farbaSpojenia);
 
@@ -60,18 +83,7 @@ export class HranaCommand implements Command{
       Informacie.plocha.remove(Informacie.vizitka);
       Informacie.poleMultipolov[this.druhy.multipol][0].off("mousedblclick");
 
-      if(this.druhy.typ_multipola == "multipol4") {
-        points.push(new fabric.Point(this.druhy.suradnicaLeft + 125, this.druhy.suradnicaTop + 125));
-      }
-      if(this.druhy.typ_multipola == "multipol2") {
-        points.push(new fabric.Point(this.druhy.suradnicaLeft + 125, this.druhy.suradnicaTop + 47.5));
-      }
-      if(this.druhy.typ_multipola == "multipol3") {
-        points.push(new fabric.Point(this.druhy.suradnicaLeft + 117.5, this.druhy.suradnicaTop + 120));
-      }
-      if(this.druhy.typ_multipola == "multipol5") {
-        points.push(new fabric.Point(this.druhy.suradnicaLeft + 117.5, this.druhy.suradnicaTop + 117.5));
-      }
+      this.set_suradnice_polyline_fakeVrchol(this.druhy,points);
       let index_of_group = Informacie.poleMultipolov[this.druhy.multipol].length-1;
       Informacie.poleMultipolov[this.druhy.multipol][0].item(0).set("fill",Informacie.poleMultipolov[this.druhy.multipol][index_of_group].farbaSpojenia);
 
@@ -83,22 +95,47 @@ export class HranaCommand implements Command{
     }
 
 
-    this.polyLine = new fabric.Polyline(points, {
-      stroke: 'black',
-      strokeWidth:5,
-      selectable:false,
-    });
-    this.polyLine.set("type","edge");          //tu bol type hrana
-    this.prvy.set("pocetHran",this.prvy.pocetHran+1);
-    this.druhy.set("pocetHran",this.druhy.pocetHran+1);
-    Informacie.plocha.add(this.polyLine);
-    Informacie.plocha.moveTo(this.polyLine, -100);
+    this.create_polyline(points);
 
-    this.prvy.lockMovementX=true;
-    this.prvy.lockMovementY=true;
-    this.druhy.lockMovementX=true;
-    this.druhy.lockMovementY=true;
-    Informacie.hranyVGrafe.push(this.hranaJS);
+
+
+  }
+
+  make_vertex_moveable(vertex){
+    vertex.lockMovementX = false;
+    vertex.lockMovementY = false;
+    vertex.item(0).set("fill",vertex.zakladnaFarba);
+    vertex.set("aktualnaFarba",vertex.zakladnaFarba);
+  }
+
+  how_many_edges_from_multipol(multipol){
+    let edges=0;
+    for(let i=1;i<Informacie.poleMultipolov[multipol].length-1;i++){
+      edges+=Informacie.poleMultipolov[multipol][i].pocetHran;
+    }
+    return edges;
+
+  }
+
+  multipol_become_moveable(multipol){
+    Informacie.poleMultipolov[multipol][0].on("mousedblclick", function (options) {
+
+      for (let i = 0; i < Informacie.poleMultipolov[this.name].length - 1; i++) {
+        Informacie.plocha.remove(Informacie.poleMultipolov[this.name][i]);
+      }
+
+      let index_of_group =Informacie.poleMultipolov[this.name].length -1;
+
+      Informacie.poleMultipolov[this.name][index_of_group].set("left", this.suradnicaLeft);
+      Informacie.poleMultipolov[this.name][index_of_group].set("top", this.suradnicaTop);
+      Informacie.plocha.add(Informacie.poleMultipolov[this.name][index_of_group]);
+      Informacie.poleMultipolov[this.name][0].item(0).set("fill", Informacie.poleMultipolov[this.name][index_of_group].zakladnaFarba);
+
+      Informacie.plocha.remove(Informacie.vizitka);
+      Informacie.plocha.renderAll();
+
+
+    });
 
 
 
@@ -114,103 +151,20 @@ export class HranaCommand implements Command{
     this.druhy.set("pocetHran",this.druhy.pocetHran-1);
     //prepocet kolko hran vedie z vchola a podla toho mu umoznime sa hybat
     if(this.prvy.pocetHran == 0 && this.prvy.type == "vertex"){
-      this.prvy.lockMovementX = false;
-      this.prvy.lockMovementY = false;
-      this.prvy.item(0).set("fill",this.prvy.zakladnaFarba);
-      this.prvy.set("aktualnaFarba",this.prvy.zakladnaFarba);
+      this.make_vertex_moveable(this.prvy);
 
     }
     if(this.druhy.pocetHran == 0 && this.druhy.type == "vertex") {
-      this.druhy.lockMovementX = false;
-      this.druhy.lockMovementY = false;
-      this.druhy.item(0).set("fill",this.druhy.zakladnaFarba);
-      this.druhy.set("aktualnaFarba",this.druhy.zakladnaFarba);
+      this.make_vertex_moveable(this.druhy);
     }
 
     //prepocet kolko hran ide z multipola a podla toho pridavame doublecklick na stred multipola
     if(this.prvy.type == "fakeVrchol"){
       Informacie.plocha.add(this.prvy);
 
-      let celkovoHran=0;
-      for(let i=1;i<Informacie.poleMultipolov[this.prvy.multipol].length-1;i++){
-        celkovoHran+=Informacie.poleMultipolov[this.prvy.multipol][i].pocetHran;
-      }
-      if(celkovoHran == 0){
-        if(this.prvy.typ_multipola == "multipol4") {
-          Informacie.poleMultipolov[this.prvy.multipol][0].on("mousedblclick", function (options) {
 
-            for (let i = 0; i < Informacie.poleMultipolov[this.name].length - 1; i++) {
-              Informacie.plocha.remove(Informacie.poleMultipolov[this.name][i]);
-            }
-
-            Informacie.poleMultipolov[this.name][5].set("left", this.suradnicaLeft);
-            Informacie.poleMultipolov[this.name][5].set("top", this.suradnicaTop);
-            Informacie.plocha.add(Informacie.poleMultipolov[this.name][5]);
-            Informacie.poleMultipolov[this.name][0].item(0).set("fill", Informacie.poleMultipolov[this.name][5].zakladnaFarba);
-
-            Informacie.plocha.remove(Informacie.vizitka);
-            Informacie.plocha.renderAll();
-
-
-          });
-        }
-        if(this.prvy.typ_multipola == "multipol5") {
-          Informacie.poleMultipolov[this.prvy.multipol][0].on("mousedblclick", function (options) {
-
-            for (let i = 0; i < Informacie.poleMultipolov[this.name].length - 1; i++) {
-              Informacie.plocha.remove(Informacie.poleMultipolov[this.name][i]);
-            }
-
-            Informacie.poleMultipolov[this.name][6].set("left", this.suradnicaLeft);
-            Informacie.poleMultipolov[this.name][6].set("top", this.suradnicaTop);
-            Informacie.plocha.add(Informacie.poleMultipolov[this.name][6]);
-            Informacie.poleMultipolov[this.name][0].item(0).set("fill", Informacie.poleMultipolov[this.name][6].zakladnaFarba);
-
-            Informacie.plocha.remove(Informacie.vizitka);
-            Informacie.plocha.renderAll();
-
-
-          });
-        }
-
-        if(this.prvy.typ_multipola == "multipol3") {
-          Informacie.poleMultipolov[this.prvy.multipol][0].on("mousedblclick", function (options) {
-
-            for (let i = 0; i < Informacie.poleMultipolov[this.name].length - 1; i++) {
-              Informacie.plocha.remove(Informacie.poleMultipolov[this.name][i]);
-            }
-
-            Informacie.poleMultipolov[this.name][4].set("left", this.suradnicaLeft);
-            Informacie.poleMultipolov[this.name][4].set("top", this.suradnicaTop);
-            Informacie.plocha.add(Informacie.poleMultipolov[this.name][4]);
-            Informacie.poleMultipolov[this.name][0].item(0).set("fill", Informacie.poleMultipolov[this.name][4].zakladnaFarba);
-
-            Informacie.plocha.remove(Informacie.vizitka);
-            Informacie.plocha.renderAll();
-
-
-          });
-        }
-
-        if(this.prvy.typ_multipola == "multipol2") {
-          Informacie.poleMultipolov[this.prvy.multipol][0].on("mousedblclick", function (options) {
-
-            for (let i = 0; i < Informacie.poleMultipolov[this.name].length - 1; i++) {
-              Informacie.plocha.remove(Informacie.poleMultipolov[this.name][i]);
-            }
-
-            Informacie.poleMultipolov[this.name][3].set("left", this.suradnicaLeft);
-            Informacie.poleMultipolov[this.name][3].set("top", this.suradnicaTop);
-            Informacie.plocha.add(Informacie.poleMultipolov[this.name][3]);
-            Informacie.poleMultipolov[this.name][0].item(0).set("fill", Informacie.poleMultipolov[this.name][3].zakladnaFarba);
-
-            Informacie.plocha.remove(Informacie.vizitka);
-            Informacie.plocha.renderAll();
-
-
-          });
-        }
-
+      if(this.how_many_edges_from_multipol(this.prvy.multipol) == 0){
+        this.multipol_become_moveable(this.prvy.multipol);
       }
     }
 
@@ -218,84 +172,12 @@ export class HranaCommand implements Command{
     if(this.druhy.type == "fakeVrchol"){
       Informacie.plocha.add(this.druhy);
 
-      let celkovoHran=0;
-      for(let i=1;i<Informacie.poleMultipolov[this.druhy.multipol].length-1;i++){
-        celkovoHran+=Informacie.poleMultipolov[this.druhy.multipol][i].pocetHran;
+      if(this.how_many_edges_from_multipol(this.druhy.multipol) == 0){
+        this.multipol_become_moveable(this.druhy.multipol);
       }
-      if(celkovoHran == 0){
-        if(this.druhy.typ_multipola == "multipol4") {
-          Informacie.poleMultipolov[this.druhy.multipol][0].on("mousedblclick", function (options) {
-
-            for (let i = 0; i < Informacie.poleMultipolov[this.name].length - 1; i++) {
-              Informacie.plocha.remove(Informacie.poleMultipolov[this.name][i]);
-            }
-
-            Informacie.poleMultipolov[this.name][5].set("left", this.suradnicaLeft);
-            Informacie.poleMultipolov[this.name][5].set("top", this.suradnicaTop);
-            Informacie.plocha.add(Informacie.poleMultipolov[this.name][5]);
-            Informacie.poleMultipolov[this.name][0].item(0).set("fill", Informacie.poleMultipolov[this.name][5].zakladnaFarba);
-
-            Informacie.plocha.remove(Informacie.vizitka);
-            Informacie.plocha.renderAll();
-          });
-        }
-        if(this.druhy.typ_multipola == "multipol5") {
-          Informacie.poleMultipolov[this.druhy.multipol][0].on("mousedblclick", function (options) {
-
-            for (let i = 0; i < Informacie.poleMultipolov[this.name].length - 1; i++) {
-              Informacie.plocha.remove(Informacie.poleMultipolov[this.name][i]);
-            }
-
-            Informacie.poleMultipolov[this.name][6].set("left", this.suradnicaLeft);
-            Informacie.poleMultipolov[this.name][6].set("top", this.suradnicaTop);
-            Informacie.plocha.add(Informacie.poleMultipolov[this.name][6]);
-            Informacie.poleMultipolov[this.name][0].item(0).set("fill", Informacie.poleMultipolov[this.name][6].zakladnaFarba);
-
-            Informacie.plocha.remove(Informacie.vizitka);
-            Informacie.plocha.renderAll();
-          });
-        }
-
-        if(this.druhy.typ_multipola == "multipol3") {
-          Informacie.poleMultipolov[this.druhy.multipol][0].on("mousedblclick", function (options) {
-
-            for (let i = 0; i < Informacie.poleMultipolov[this.name].length - 1; i++) {
-              Informacie.plocha.remove(Informacie.poleMultipolov[this.name][i]);
-            }
-
-            Informacie.poleMultipolov[this.name][4].set("left", this.suradnicaLeft);
-            Informacie.poleMultipolov[this.name][4].set("top", this.suradnicaTop);
-            Informacie.plocha.add(Informacie.poleMultipolov[this.name][4]);
-            Informacie.poleMultipolov[this.name][0].item(0).set("fill", Informacie.poleMultipolov[this.name][4].zakladnaFarba);
-
-            Informacie.plocha.remove(Informacie.vizitka);
-            Informacie.plocha.renderAll();
 
 
-          });
-        }
 
-        if(this.druhy.typ_multipola == "multipol2") {
-          Informacie.poleMultipolov[this.druhy.multipol][0].on("mousedblclick", function (options) {
-
-            for (let i = 0; i < Informacie.poleMultipolov[this.name].length - 1; i++) {
-              Informacie.plocha.remove(Informacie.poleMultipolov[this.name][i]);
-            }
-
-            Informacie.poleMultipolov[this.name][3].set("left", this.suradnicaLeft);
-            Informacie.poleMultipolov[this.name][3].set("top", this.suradnicaTop);
-            Informacie.plocha.add(Informacie.poleMultipolov[this.name][3]);
-            Informacie.poleMultipolov[this.name][0].item(0).set("fill", Informacie.poleMultipolov[this.name][3].zakladnaFarba);
-
-            Informacie.plocha.remove(Informacie.vizitka);
-            Informacie.plocha.renderAll();
-
-
-          });
-        }
-
-
-      }
     }
 
 
