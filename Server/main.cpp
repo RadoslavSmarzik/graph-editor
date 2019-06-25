8762ab6fd4687d53eec1d33df7aa93aec6b425f1
Filip Novak <filip.novak11@gmail.com>
2019-06-25 08:31:42 +0200

    Finalna verzia

--- Server/main.cpp
+++ Server/main.cpp
@@ -6,16 +6,12 @@
  * 
  */
 
-/*#include <snarks/colouring_pd.hpp>
-#include <graphs/snarks.hpp>
-#include <snarks/colouring_pd.hpp>*/ 
-
 #include <basic_impl.hpp>
 #include <invariants.hpp>
 #include <graphs.hpp>
 #include <operations.hpp>
 #include <io.hpp>
-//#include <algorithms/cyclic_connectivity.hpp>//
+//#include <algorithms/cyclic_connectivity.hpp>
 
 #include <config/configuration.hpp>
 #include <util/json.hpp>
@@ -23,7 +19,6 @@
 #include "httprequestparser.h"
 #include "request.h"
 
-#include <cassert>
 #include <iomanip>
 
 #include <sstream>
@@ -44,53 +39,41 @@ using namespace std;
 using namespace ba_graph;
 using namespace httpparser;
 
-struct Response { 																	
+struct Response {
     int versionMajor = 1;
     int versionMinor = 1;
     unsigned int statusCode1 = 200;
     unsigned int statusCode2 = 404;
-	unsigned int statusCode3 = 400;
+    unsigned int statusCode3 = 400;
     std::string status1 = "OK";
     std::string status2 = "Not Found";
-	std::string status3 = "Bad Request";
+    std::string status3 = "Bad Request";
     std::vector<pair<std::string, std::string>> headers = {{"Access-Control-Allow-Origin", "http://localhost:4200"}};
 };
 
-std::string make_response(int num, std::string cType = "", std::string content = "") {				//mozem mat dva nulove stringy ?
+std::string make_response(int num, std::string cType = "", std::string content = "") {
     Response res;
     if (cType == "plain") {
         res.headers.push_back({"Content-Type", "text/plain"});
     } else if (cType == "json") {
         res.headers.push_back({"Content-Type", "application/json"});
-    } /*else if (cType == "html") {
-        res.headers.push_back({"Content-Type", "text/html"});
-    }*/
+    }
     std::stringstream stream;
     if (num == 1) {
         stream << "HTTPS/" << res.versionMajor << "." << res.versionMinor
                << " " << res.statusCode1 << " " << res.status1 << "\n";
-        
-		for (pair<std::string, std::string> i : res.headers) {
-            stream << i.first << ": " << i.second << "\n";
-        }
-        stream << "\n" << content;
     } else if (num == 2) {
         stream << "HTTPS/" << res.versionMajor << "." << res.versionMinor
                << " " << res.statusCode2 << " " << res.status2 << "\n";
-        
-		for (pair<std::string, std::string> i : res.headers) {
-            stream << i.first << ": " << i.second << "\n";
-        }
-        stream << "\n" << content;
     } else if (num == 3) {
-		stream << "HTTPS/" << res.versionMajor << "." << res.versionMinor
+        stream << "HTTPS/" << res.versionMajor << "." << res.versionMinor
                << " " << res.statusCode3 << " " << res.status3 << "\n";
-        
-		for (pair<std::string, std::string> i : res.headers) {
-            stream << i.first << ": " << i.second << "\n";
-        }
-        stream << "\n" << content;
     }
+    for (pair<std::string, std::string> i : res.headers) {
+        stream << i.first << ": " << i.second << "\n";
+    }
+    stream << "\n" << content;
+
     return stream.str();
 }
 
@@ -103,15 +86,14 @@ json datab_mult() {
 }
 
 class GraphBuilder {
-
-    Graph g = createG(); 
+    Graph g = createG();
     json info;
     json array_of_mult;
     std::map<int, Number> map_of_vert;
     std::map<pair<int, std::string>, Number> map_of_mult;
 
 public:
-	GraphBuilder(json data) {
+    GraphBuilder(json data) {
         array_of_mult = datab_mult();
         info = data;
     }
@@ -119,10 +101,10 @@ public:
     Graph & make_graph() {
         int c = 0;
         for (auto& v : info["vertices"]) {
-            c++;
             int vertex = std::stoi(v.get<std::string>());
             map_of_vert[vertex] = c;
             addV(g, c);
+            c++;
         }
         for (auto& m : info["multipoles"]) {
             for (auto& a_o_m : array_of_mult) {
@@ -179,7 +161,7 @@ int main(int argc, char const *argv[]) {
 
     int server_fd, new_socket;
     struct sockaddr_in address;
-    int opt = 1;
+    //int opt = 1;
     int addrlen = sizeof (address);
     char buffer[800000] = {0};
 
@@ -196,67 +178,71 @@ int main(int argc, char const *argv[]) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
-    if (listen(server_fd, 3) < 0) { //pocuva az 3 requesty naraz
+    if (listen(server_fd, 3) < 0) {
         perror("listen");
         exit(EXIT_FAILURE);
     }
 
-    while (true) { 
+    while (true) {
 
         if ((new_socket = accept(server_fd, (struct sockaddr *) &address, (socklen_t*) & addrlen)) < 0) {
             perror("accept");
             exit(EXIT_FAILURE);
         }
-	
+
         read(new_socket, buffer, 800000);
-        printf("%s\n", buffer); //
+        std::cout << std::endl << "Incoming Request:" << std::endl;
+        std::cout << buffer << std::endl;
 
         Request request;
         HttpRequestParser parser;
         HttpRequestParser::ParseResult res = parser.parse(request, buffer, buffer + sizeof (buffer));
 
-        if (res == HttpRequestParser::ParsingCompleted) {				
-
-            std::cout << request.method << std::endl; //
-            std::cout << request.uri << std::endl; //
+        if (res == HttpRequestParser::ParsingCompleted) {
 
             std::string data(request.content.begin(), request.content.end());
-            std::cout << data << std::endl; //
 
             if (request.method == "POST") {
                 if (request.uri.compare(0, 6, "/graph") == 0) {
-					if (data.empty()) {														
-						Graph g = createG();
-						std::string s =	write_sparse6(g);
-						
-						std::string str = make_response(1, "plain", s);
-						const char * resp = str.c_str();
-						send(new_socket, resp, strlen(resp), 0);
-						close(new_socket);
-					} else {						
-						json info = json::parse(data);
-						GraphBuilder gb(info);
-						try {
-							Graph &g = gb.make_graph();
-							std::string s =	write_sparse6(g);
-		
-							std::string str = make_response(1, "plain", s);
-							const char * resp = str.c_str();
-							send(new_socket, resp, strlen(resp), 0);	
-							close(new_socket);
-						}
-						catch(const std::invalid_argument& e) {
-							std::string str = make_response(2);
-							const char * resp = str.c_str();
-							send(new_socket, resp, strlen(resp), 0);
-							close(new_socket);						
-						}
-					}	
+                    if (data.empty()) {
+                        Graph g = createG();
+                        std::string s = write_sparse6(g);
+
+                        std::string str = make_response(1, "plain", s);
+                        const char * resp = str.c_str();
+                        send(new_socket, resp, strlen(resp), 0);
+                        std::cout << std::endl << "Response sent:" << std::endl;
+                        std::cout << str << std::endl;
+                        close(new_socket);
+                    } else {
+                        json info = json::parse(data);
+                        GraphBuilder gb(info);
+                        try {
+                            Graph &g = gb.make_graph();
+                            std::string s = write_sparse6(g);
+
+                            std::string str = make_response(1, "plain", s);
+                            const char * resp = str.c_str();
+                            send(new_socket, resp, strlen(resp), 0);
+                            std::cout << std::endl << "Response sent:" << std::endl;
+                            std::cout << str << std::endl;
+                            close(new_socket);
+                        } catch (const std::invalid_argument& e) {
+                            std::string str = make_response(2);
+                            const char * resp = str.c_str();
+                            send(new_socket, resp, strlen(resp), 0);
+                            std::cout << std::endl << "Response sent:" << std::endl;
+                            std::cout << str << std::endl;
+                            close(new_socket);
+                        }
+                    }
                 } else {
                     string str = make_response(2);
                     const char * resp = str.c_str();
                     send(new_socket, resp, strlen(resp), 0);
-					close(new_socket);
+                    std::cout << std::endl << "Response sent:" << std::endl;
+                    std::cout << str << std::endl;
+                    close(new_socket);
                 }
             } else if (request.method == "GET") {
                 if (request.uri.compare(0, 9, "/multipol") == 0) {
@@ -266,42 +252,45 @@ int main(int argc, char const *argv[]) {
                     std::string str = make_response(1, "json", s);
                     const char * resp = str.c_str();
                     send(new_socket, resp, strlen(resp), 0);
-					close(new_socket);
+                    std::cout << std::endl << "Response sent:" << std::endl;
+                    std::cout << str << std::endl;
+                    close(new_socket);
                 } else if (request.uri.compare(0, 11, "/invariants") == 0) {
-					std::string graph_code = request.uri.substr(12);
+                    std::string graph_code = request.uri.substr(12);
                     Graph g = read_graph6_line(graph_code);
                     json invariants;
-                    invariants["girth"] = girth(g); 											//treba dat do stringu asi
+                    invariants["girth"] = girth(g); 		//mozno dat do stringu
                     invariants["max_deg"] = max_deg(g);
                     invariants["min_deg"] = min_deg(g);
-					invariants["chromatic_index"] = chromatic_index_basic(g);
-					/*if(invariants["max_deg"] == 3 && invariants["min_deg"] == 3){
-						invariants["cyclic_connectivity"] = cyclic_connectivity(g);
+                    invariants["chromatic_index"] = chromatic_index_basic(g);
+                    /*if(invariants["max_deg"] == 3 && invariants["min_deg"] == 3){
+                            invariants["cyclic_connectivity"] = cyclic_connectivity(g);
                     }*/
 
                     std::string s = invariants.dump();
-                    std::string str = make_response(1, "json", s); 								
+                    std::string str = make_response(1, "json", s);
                     const char * resp = str.c_str();
                     send(new_socket, resp, strlen(resp), 0);
-					close(new_socket);
+                    std::cout << std::endl << "Response sent:" << std::endl;
+                    std::cout << str << std::endl;
+                    close(new_socket);
                 } else {
                     string str = make_response(2);
                     const char * resp = str.c_str();
                     send(new_socket, resp, strlen(resp), 0);
-					close(new_socket);
+                    std::cout << std::endl << "Response sent:" << std::endl;
+                    std::cout << str << std::endl;
+                    close(new_socket);
                 }
             }
         } else {
             string str = make_response(3);
             const char * resp = str.c_str();
             send(new_socket, resp, strlen(resp), 0);
-			close(new_socket);
+            std::cout << std::endl << "Response sent:" << std::endl;
+            std::cout << str << std::endl;
+            close(new_socket);
         }
     }
     return 0;
 }
-
-
-
-
-
